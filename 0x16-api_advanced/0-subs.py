9831a139eefc79d4c48ba924811eb570e265c00f
m-lack <immlack@gmail.com>
2025-03-20 08:17:33 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return "OK"  # Return "OK" for invalid subreddit input
-
-    try:
-        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                         headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-
-        # Check if the subreddit exists
-        if r.get("error"):
-            return "OK"  # Return "OK" if subreddit does not exist
-
-        subs = r.get("data", {}).get("subscribers", 0)
-        return subs  # Return the number of subscribers if subreddit exists
-    except Exception:
-        return "OK"  # Return "OK" in case of an error (e.g., network issues)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
 
